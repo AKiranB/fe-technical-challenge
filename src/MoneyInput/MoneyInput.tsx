aa740e9f771b25ab4628e2f54d78b785ebc1a2ea
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-11-02 12:20:31 +0100

    Create basic controlled component with onChange handler

--- src/MoneyInput/MoneyInput.tsx
+++ src/MoneyInput/MoneyInput.tsx
@@ -1,10 +1,22 @@
-import _styles from './MoneyInput.module.css'
-
-interface MoneyInput extends React.InputHTMLAttributes<HTMLInputElement> {
-  locale: string
-  error: boolean
+interface MoneyInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  locale?: string
+  error?: boolean
+  onValueChange: (value: string) => void
+  value: string
 }
 
-export default function MoneyInput({ locale, error = false, ...props }: MoneyInput): JSX.Element {
-  return <input {...props} />
+export default function MoneyInput({
+  locale,
+  error = false,
+  value,
+  onValueChange,
+  ...props
+}: MoneyInputProps): JSX.Element {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //TODO: handle locale, handle formatting
+    const { value } = event.target
+    onValueChange(value)
+  }
+
+  return <input value={value} onChange={handleChange} {...props} />
 }
